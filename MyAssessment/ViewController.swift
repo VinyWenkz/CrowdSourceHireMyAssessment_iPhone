669b878bc67af3df8018abd9105391fd157d0c13
Divine Bendicion <Divine.Bendicion@cloudsherpas.com>
2015-04-11 22:49:49 +0800

    Initial Commit

---
+++ MyAssessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAssessment
+//
+//  Created by DGBendicion on 4/11/15.
+//  Copyright (c) 2015 DGBendicion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
